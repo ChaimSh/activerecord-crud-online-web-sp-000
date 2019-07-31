e3251e98210347c490f9a594d244bc7953ff4cf5
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-31 02:35:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,8 +30,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = "The Room"
+    m.release_date = 2003
   end
 end
 
